8163af81cc3ecf9013f6828e8a0786cabbdf0614
Camilo Henrique <camilo.henrique@pecicero.com.br>
2024-04-22 15:52:59 -0300

    feat: aula 35

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { CardRoxoComponent } from './card-roxo/card-roxo.component';
-import { CardButtonComponent } from './card-button/card-button.component';
 import { CardsModule } from './cards/cards.module';
 
 @NgModule({
